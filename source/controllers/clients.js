9d6a20eb706b6168b2765cf5db985cb95a8a0ae3
George Czabania <george@czabania.com>
2014-04-25 15:39:34 +1200

    Clean up clients page styles

--- source/controllers/clients.js
+++ source/controllers/clients.js
@@ -5,35 +5,29 @@ var ClientsPage = require('../views/pages/clients');
 var ClientsController = function () {
   this.clients = new ClientsCollection();
 
-  this.clients.add({
-    name: 'Captain Jack Sparrow',
-    address: 'The Black Pearl',
-    city: 'Tortuga',
-    postcode: '1234'
-  });
+  for (var i = 0; i < 10; i++) {
+    this.clients.add({
+      name: 'Captain Jack Sparrow',
+      address: 'The Black Pearl',
+      city: 'Tortuga',
+      postcode: '1234'
+    });
+  }
 
   var invoices = this.clients.first().get('invoices');
-  console.log(invoices);
 
-  invoices.add({
-    id: 7300,
-    created: new Date(),
-    date: new Date(),
-    customer: 'Will Turner',
-    email: 'will@gmail.com',
-    site: 'The Armory',
-    cost: 200
-  });
+  for (i = 0; i < 10; i++) {
+    invoices.add({
+      id: 7300 + i,
+      created: new Date(),
+      date: new Date(),
+      customer: 'Will Turner',
+      email: 'will@gmail.com',
+      site: 'The Armory',
+      cost: 200
+    });
+  }
 
-  invoices.add({
-    id: 7301,
-    created: new Date(),
-    date: new Date(),
-    customer: 'Bill Turner',
-    email: 'bill@gmail.com',
-    site: 'The Pirate Ship',
-    cost: 30.21
-  });
 
 };
 
